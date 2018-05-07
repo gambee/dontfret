c12aadc1072a0c4da61bb441ec90d78fe1fe02cb
mgambee <max.gambee@intel.com>
2018-05-06 18:24:41 -0700

    added funcioning svg (xml) document writing classes fretboad is now rendering

--- diagram_compiler/diagcomp.py
+++ diagram_compiler/diagcomp.py
@@ -1,12 +1,21 @@
 import re
 import yaml
+from docwriter import *
 
-fd_properties = {
-        'title',
+fd_required = {
         'string_count',
-        'fret_range',
-        'fingerings'
+        'fret_range'
         }
+
+fd_optional = {
+        'title',
+        'fingerings',
+        'base_width',
+        'ratio',
+        'offset'
+        }
+
+fd_properties = fd_required.union(fd_optional)
 
 class FretDiagram:
     def __init__(self, yaml_dict={}, **kwargs):
@@ -15,22 +24,152 @@ class FretDiagram:
         args.update(yaml_dict)
         args.update(kwargs)
 
-        if 'title' in args:
-            self.title = args['title']
-        if 'string_count' in args:
-            self.string_count = args['string_count']
-        if 'fret_range' in args:
-            self.fret_min = args['fret_range'][0]
-            self.fret_max = args['fret_range'][1]
-        if 'fingerings' in args:
-            self.fingerings = args['fingerings']
-
         if(not set(args) <= fd_properties):
             t = "["
             for i in set(args) - fd_properties:
                t += "'" + i + "', "
             t = t[:-2] + "]"
             print("Uknown parameters: " + t)
+            raise Exception('Uknown parameters')
+
+        if(not set(args) <= fd_properties):
+            t = "["
+            for i in set(args) - fd_properties:
+               t += "'" + i + "', "
+            t = t[:-2] + "]"
+            print("Missing required parameters: " + t)
+            raise Exception('Missing required parameters')
+
+        # Required Parameters
+        self.string_count = args['string_count']
+        self.fret_min = args['fret_range'][0]
+        self.fret_max = args['fret_range'][1]
+        for i in {self.string_count, self.fret_min, self.fret_max}:
+            if(not isinstance(i, int)):
+                raise Exception('Type oops!!')
+
+        # Optional Parameters
+        if 'fingerings' in args:
+            self.fingerings = args['fingerings']
+        else:
+            self.fingerings = []
+
+        if 'title' in args:
+            self.title = args['title']
+        else:
+            self.title = ""
+
+        if 'base_width' in args:
+            self.base_width = args['base_width']
+        else:
+            self.base_width = 100
+
+        if 'ratio' in args:
+            self.ratio = args['ratio']
+        else:
+            self.ratio = 1.5
+
+        if 'offset' in args:
+            self.offset = args['offset']
+        else:
+            self.offset = 50
 
     def __str__(self):
+        # establish geometry
+        width = self.offset * 2 + (self.string_count - 1) * self.base_width
+        height = self.offset * 2 + (self.fret_max - self.fret_min) * \
+                    self.ratio * self.base_width
+        vbox = [-self.offset,
+                -self.offset,
+                width + self.offset,
+                height + self.offset]
 
+        circle = XMLTag({
+           'tagname': 'circle',
+           'properties': [
+               Property('cx', '25'),
+               Property('cy', '25'),
+               Property('r', '25')
+               ],
+           'closed': 'True'
+           })
+
+        symbol = XMLTag({
+           'tagname': 'symbol',
+           'properties': [
+               Property('id', 'circ')
+               ],
+           'contents': [circle]
+           })
+
+        g = XMLTag({
+           'tagname': 'g',
+           'contents': [symbol]
+           })
+
+        defs = XMLTag({
+           'tagname': 'defs',
+           'contents': [g]
+           })
+
+        strings = []
+        for i in range(self.string_count):
+           strings += [
+               XMLTag({
+                   'tagname': 'line',
+                   'closed': 'True',
+                   'properties': [
+                       Property('x1', str(self.base_width * i)),
+                       Property('y1', str(0)),
+                       Property('x2', str(self.base_width * i)),
+                       Property('y2', str((self.fret_max - self.fret_min) * self.ratio * \
+                               self.base_width)),
+                       Property('stroke', 'black'),
+                       Property('stroke-width', '2')
+                       ]
+                   })
+               ]
+
+        frets = []
+        for i in range(self.fret_max - self.fret_min + 1):
+           frets += [
+                XMLTag({
+                    'tagname': 'line',
+                    'closed': 'True',
+                    'properties': [
+                       Property('x1', str(0)),
+                       Property('y1', str(self.ratio * self.base_width * i)),
+                       Property('x2', str(self.base_width * (self.string_count - 1))),
+                       Property('y2', str(self.ratio * self.base_width * i)),
+                       Property('stroke', 'black'),
+                       Property('stroke-width', '2')
+                       ]
+                    })
+                ]
+
+        # Check if nut is to be rendered
+        if(self.fret_min == 0):
+            frets[0]['stroke-width'] = '10'
+            frets[0]['stroke-linecap'] = 'square'
+
+        svg = XMLTag({
+            'tagname': 'svg',
+            'properties': [
+                Property('width', str(width)),
+                Property('height', str(height)),
+                Property(
+                    'viewBox',
+                    str(vbox[0]) + ' ' + str(vbox[1]) + ' ' 
+                        + str(vbox[2]) + ' ' + str(vbox[3]))
+                    ],
+            'contents': [defs] + frets + strings
+                })
+
+        html = HtmlDocument([svg])
+        
+        return str(html)
+
+d = yaml.load(open('example.yaml').read())
+fd = FretDiagram(d)
+with open('out.html', 'w') as f:
+    f.write(str(fd))
---
+++ diagram_compiler/docwriter.py
@@ -0,0 +1,127 @@
+import re
+
+class Property:
+    def __init__(self, name, value=""):
+        self.name = name
+        self.value = value
+
+    def __str__(self):
+        return self.name + '="' + self.value + '"'
+
+def tobool(s):
+    if(isinstance(s, str)):
+        if(bool(re.match('(t(rue)?|y(es)?)', s, re.IGNORECASE))):
+            return True
+        elif(bool(re.match('(f(alse)?|n(o)?)', s, re.IGNORECASE))):
+            return False
+        else:
+            raise Exception('tobool: unknown format string')
+    else:
+        return bool(s)
+
+
+class DocElement:
+    def __init__(self, initargs={}):
+        if('header' in initargs):
+            self.header = initargs['header']
+        else:
+            self.header = ""
+
+        if('footer' in initargs):
+            self.footer = initargs['footer']
+        else:
+            self.footer = ""
+
+        if('contents' in initargs):
+            self.contents = initargs['contents']
+        else:
+            self.contents = []
+
+    def __str__(self):
+        return self._str_(0)
+
+    def _str_(self, depth):
+        indent = ""
+        for i in range(depth):
+            indent += "  "
+        s = indent + self.header + '\n'
+        for element in self.contents:
+            s += element._str_(depth + 1) + '\n'
+        s += indent + self.footer
+        return s
+
+html_header = '''<!DOCTYPE html>
+<html>
+<head>
+<title>HtmlElement</title>
+</head>
+<body>'''
+
+class HtmlDocument(DocElement):
+    def __init__(self, initargs=[]):
+        args = {
+            'header': html_header,
+            'footer': '</body>\n</html>',
+            'contents': initargs
+            }
+
+
+        super().__init__(args)
+
+class XMLTag(DocElement):
+    def __init__(self, initargs={}):
+        if('tagname' in initargs):
+            self.tagname = initargs['tagname']
+        else:
+            self.tagname = '?unkown?'
+
+        if('properties' in initargs):
+            self.properties = initargs['properties']
+        else:
+            self.properties = []
+        
+        if('contents' in initargs):
+            self.contents = initargs['contents']
+        else:
+            self.contents = []
+
+        if('closed' in initargs):
+            self.closed = tobool(initargs['closed'])
+        else:
+            self.closed = False
+
+    def __getitem__(self, key):
+        props = []
+        for p in self.properties:
+            props += [(p.name, p.value)]
+        props = dict(props)
+        if(key in props):
+            return props[key]
+        else:
+            return None
+
+    def __setitem__(self, key, value):
+        for p in self.properties:
+            if(key == p.name):
+                p.value = value
+                return
+        self.properties += [Property(key, value)]
+
+    def __delitem__(self, key):
+        tmp = []
+        for p in self.properties:
+            if(p.name != key):
+                tmp += [p]
+        self.properties = tmp
+
+    def _str_(self, depth):
+        self.header = '<' + self.tagname
+        for p in self.properties:
+            self.header += ' ' + str(p)
+        if(self.closed):
+            self.header += '/>'
+            self.footer = ''
+        else:
+            self.header += '>'
+            self.footer = '</' + self.tagname + '>'
+        return super()._str_(depth)
